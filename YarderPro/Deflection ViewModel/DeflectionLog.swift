69f46ffb67f4f40df29841cd021675597a6ce55a
DrewHengehold <drew.hengehold@gmail.com>
2024-10-08 22:05:54 -0700

    Putting Apple YarderPro on the Map

---
+++ YarderPro/Deflection ViewModel/DeflectionLog.swift
@@ -0,0 +1,29 @@
+//
+//  DeflectionLog.swift
+//  Forestry
+//
+//  Created by Drew Hengehold on 9/24/24.
+//
+import Foundation
+import Combine
+
+class DeflectionLog: ObservableObject, Identifiable{
+    //Each log must be unique to be listed so we need a unique ID
+    var id = UUID()
+    // All the variables used in the detail tab
+    @Published var logName: String = ""
+    @Published var logDate: Date = Date()
+    @Published var description: String = ""
+    @Published var southCoord: Double? = nil
+    @Published var northCoord: Double? = nil
+    @Published var eastCoord: Double? = nil
+    @Published var westCoord: Double? = nil
+    
+    //All the variables used in the deflection calculator tab
+    
+    //All variables for the tension tab
+    
+    //All variables for the Diagram Tap
+    
+    //All Variables for the Map Tab
+}
